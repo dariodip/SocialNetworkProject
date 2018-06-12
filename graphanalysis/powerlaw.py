d7a6098df0bda94f0903314fe9f879f7a9e57ba0
mattiatomeo <tomeo.mat@gmail.com>
2018-06-12 12:51:43 +0200

    powerlaw distribution graphic and requirements updated

--- graphanalysis/powerlaw.py
+++ graphanalysis/powerlaw.py
@@ -1,21 +1,38 @@
 import powerlaw
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
+np.seterr(divide='ignore', invalid='ignore')
 
-def get_powerlaw_distribution(g):
-    degree = []
+def get_node_degree(g):
     if nx.is_directed(g):
         degree = [t[1] for t in g.in_degree]
     else:
         degree = [t[1] for t in g.degree]
 
-    return powerlaw.Fit(degree, xmin=min(degree), xmax=max(degree))
+    return degree
+
+def get_powerlaw_distribution_alpha(g):
+    degree = get_node_degree(g)
+
+    fit = powerlaw.Fit(degree, xmin=min(degree), xmax=max(degree))
+    return fit.power_law.alpha
 
 def plot_degree_powerlaw_distribution(g):
-    fit = get_powerlaw_distribution(g)
+    # Degree frequencies
+    degree = pd.Series(get_node_degree(g))
+    degree_count = degree.value_counts()
+    degree_freq = {d: degree_count[d]/sum(degree_count) for d in degree_count.index}
+    xvalue = list(degree_freq.keys())
+    yvalue= list(degree_freq.values())
+    plt.plot(xvalue, yvalue, 'ro')
 
-    fig = fit.plot_pdf(color='r', marker="o", linestyle="None", original_data=True)
-    fit.power_law.plot_pdf(color='b', linestyle='--', ax=fig)
-
+    # Powerlaw distribution
+    alpha = get_powerlaw_distribution_alpha(g)
+    print(min(degree_count.index), max(degree_count.index))
+    xvalue = np.linspace(min(degree_count.index), max(degree_count.index), 100)
+    yvalue = list(map(lambda x: x**(-alpha), xvalue))
+    plt.plot(xvalue, yvalue, 'b-')
     plt.show()
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import graphanalysis.loader as loader
 import graphanalysis.graphanalyser as ga
 import pprint
-import networkx as nx
+
 from graphanalysis import influence
+from graphanalysis.powerlaw import plot_degree_powerlaw_distribution
 
 if __name__ == '__main__':
     g = loader.load('./resources/dataset/karate-club.txt', is_directed=True, sep=" ")
     pprint.pprint(ga.GraphAnalyser(g).get_properties(), width=500)
-
     print("target set:", influence.generate_target_set_selection(g, 1))
+
+    # Powerlaw graph
+    plot_degree_powerlaw_distribution(g)
