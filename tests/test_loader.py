3fc11788db3db9b5fc005b405077921b1f02d2fc
mattiatomeo <tomeo.mat@gmail.com>
2018-06-14 17:47:30 +0200

    loader test dataset changed The dataset now is CA-GrQc Now the loader test check the import of both directed and undirected case

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -5,17 +5,26 @@ import graphanalysis.loader as loader
 
 class GraphAnalysisLoaderTest(unittest.TestCase):
     def test_loader(self):
-        dataset = "resources/dataset/wiki-Vote.txt"
+        dataset = "resources/dataset/CA-GrQc.txt"
 
         exists = os.path.exists(dataset)
         self.assertTrue(exists, "{} does not exists".format(dataset))  # Check if we give the right path
 
-        g = loader.load(dataset, is_directed=True)
+        g = loader.load(dataset, is_directed=False)
         self.assertIsNotNone(g, "the graph cannot be None")
 
-        expectedNodes = 7115  # Nodes contained in wiki-Vote.txt
+        expectedNodes = 5242  # Nodes contained in wiki-Vote.txt
         self.assertEqual(len(g.nodes()), expectedNodes,
                          "Missing some nodes. Found {}, expected {}".format(len(g.nodes), expectedNodes))
-        expectedEdges = 103689  # Edges contained in wiki-Vote.txt
+        expectedEdges = 14496  # Edges contained in wiki-Vote.txt
         self.assertEqual(len(g.edges()), expectedEdges,
+                         "Missing some edges. Found {}, expected {}".format(len(g.edges), expectedEdges))
+
+        g = loader.load(dataset, is_directed=True)
+
+        self.assertEqual(len(g.nodes()), expectedNodes,
+                         "Missing some nodes. Found {}, expected {}".format(len(g.nodes), expectedNodes))
+        # if we suppose that CA-GrQc is directed, then each edge must be counted twice
+        expectedDirectedEdges = 28980
+        self.assertEqual(len(g.edges()), expectedDirectedEdges,
                          "Missing some edges. Found {}, expected {}".format(len(g.edges), expectedEdges))
